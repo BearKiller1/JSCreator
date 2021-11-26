7630fff8e1cf72bec4323678e6ff315ee24c59b3
uchaiaucha <ucha1bokeria.gmail.com>
2021-11-26 18:59:04 +0400

    height update

--- index.js
+++ index.js
@@ -2,30 +2,44 @@ onload = () => {
     var button = document.getElementById("create");
 
     button.onclick = () => {
-
-        var element = document.getElementById("element").value;
-        var class_name = document.getElementById("class_name").value;
+        var color       =   document.getElementById("color").value;
+        var width       =   document.getElementById("width").value;
+        var height      =   document.getElementById("height").value;
+        var element     =   document.getElementById("element").value;
+        var class_name  =   document.getElementById("class_name").value;
+        var inner_html  =   document.getElementById("inner_html").value;
         var id_name = document.getElementById("id_name").value;
         var id_checker = !!document.getElementById(id_name);
 
         if(!id_checker){
-            var inner_html = document.getElementById("inner_html").value;
-            var color = document.getElementById("color").value;
-            console.log(color);
-    
             var html = document.createElement(element);
+            var styles = {
+                "background-color": color,
+                "width": width+"px",
+                "height": height+"px"
+            };
+            Object.assign(html.style, styles);
+
             html.innerHTML = inner_html;
-            html.style.background = color;
+
             html.setAttribute('class',class_name);
             html.setAttribute('id',id_name);
     
             document.getElementById("output").appendChild(html);
         }
-        else{
-            var dom = document.getElementById(id_name);
-            console.log(dom);
-            dom.style.display = "none";
-        }
+        // else{
+        //     var html = document.getElementById(id_name);
+            
+        //     var styles = {
+        //         "background-color": color,
+        //         "width": width+"px",
+        //         "height": height+"px"
+        //     };
+        //     Object.assign(html.style, styles);
+            
+        //     html.innerHTML = inner_html;
+                
+        // }
     }
 };
 
